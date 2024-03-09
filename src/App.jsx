b1e6bc0875bdcf79d91cde57603662e85a608ab8
nikhil-gowda-t-p <nikhilgowda13121@gmail.com>
2024-03-09 11:56:37 +0530

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { BrowserRouter ,Route,Routes } from 'react-router-dom'
+import Home from './newpages/Home';
+
+import Signup from './newpages/Signup';
+import Sout from './newpages/Sout';
+import Profile from './newpages/Profile';
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/profile" element={<Profile />}/>
+      <Route path="/signup" element={<Signup />}/>
+      <Route path="/signout" element={<Sout />}/>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
